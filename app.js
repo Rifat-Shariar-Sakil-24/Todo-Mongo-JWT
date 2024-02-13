704ed6a1da1152815900bd7abf1e2c77a7346a45
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2024-02-13 22:14:11 +0600

    db final

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require('./models/users');
 
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-connectDB().then(console.log("connected to DB Online")).catch(err => console.log(err));
+const url = 'mongodb+srv://'+process.env.DBNAME+':'+process.env.DBPASS+'@cluster0.qrtll88.mongodb.net/ToDoList'
+connectDB().then(console.log("connected to DB")).catch(err => console.log(err));
 async function connectDB() {
-    await mongoose.connect('mongodb+srv://'+ process.env.DBNAME + ':' + process.env.DBPASS+'@cluster0.qrtll88.mongodb.net/ToDoList');
+    await mongoose.connect(url);
   
   }
 connectDB();
 
+
+
+app.get("/",function(req,res){
+   
+})
+
 app.listen(4000,function(){
     console.log("server is running on port 4000");
 })
+
