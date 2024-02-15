58ed83a2d9279b85f57a29641b4301c8d5bf0c3a
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2024-02-16 01:17:04 +0600

    heroku

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/logout',function(req,res){
   res.redirect('/');
 })
 
-app.listen(4000,function(){
+app.listen(process.env.PORT || 4000,function(){
     console.log("server is running on port 4000");
 })
 
